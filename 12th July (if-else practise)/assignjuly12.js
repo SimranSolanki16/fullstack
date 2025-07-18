0cfba30320c8dfbaa45b3945785dda2a1b8a9e0a
SimranSolanki16 <simransolanki1604@gmail.com>
2025-07-18 16:06:31 +0530

    Add files via upload

---
+++ 12th July (if-else practise)/assignjuly12.js
@@ -0,0 +1,96 @@
+const prompt = require('prompt-sync')()
+
+// let n=parseInt(prompt("enter a no."))
+// if(n%2==0){
+//     console.log('no. is even')
+// }
+// else{
+//     console.log('no. is odd')
+// }
+
+// if(n>0){
+//     console.log('no.is positive')
+// }
+// else if(n=0){
+//     console.log('No.is zero')
+// }
+// else{console.log('no. is negative')}
+
+// age=parseInt(prompt('enter your age'))
+// if (age>=18){
+//     console.log("you can vote")
+// }
+// else{
+//     console.log("you are not allowed to vote")
+// } 
+
+
+// range=parseInt(prompt('enter a no.'))
+// if (range>=10 && range<=20){
+//     console.log("In range")
+// }
+// else{
+//     console.log("out of range")
+// }
+
+// a=parseInt(prompt('enter first No.'))
+// b=parseInt(prompt('enter second No.'))
+// if(a==b){
+//     console.log('nos. are equal ')
+// }
+// else if(a>b){
+//     console.log('nos. are not equal First no. is larger' )
+// }
+// else{
+//     console.log('nos. are not equal Second no. is larger')
+// }
+
+// let p = parseInt(prompt('enter your percentage : -  '))
+
+// if(p>=0 && p <=49){
+//     console.log('fail')
+// }
+// else if(p>=50 && p <=74){
+//     console.log('grade c')
+// }
+// else if(p>=75 && p <=89){
+//     console.log('grade b')
+// }
+// else if(p>=90 && p <=100){
+//     console.log('grade a')
+// }
+// else {
+//     console.log('not b/w 0-100')
+// }
+
+// let c=parseInt(prompt('enter a no.' ))
+// if(c%5==0 && c%11==0){
+//     console.log('no. is divisible by 5 and 11')
+// }
+// else{
+//     console.log('not divisible by both 5 and 11')
+// }
+
+// let e=parseInt(prompt('enter first No.'))
+// let f=parseInt(prompt('enter second No.'))
+// let g=parseInt(prompt('enter third No.'))
+// if(e>f && e>g){
+//     console.log('first no. is larger ')
+// }
+// else if(f>e && f>g){
+//     console.log('second no. is larger' )
+// }
+// else{
+//     console.log('third no. is larger')
+// }
+
+
+// let leapYear=parseInt(prompt('Enter the year'))
+// if(leapYear%4 == 0 && leapYear%10 != 0 ) console.log('Leap year')
+//     else console.log('Not a leap year')
+
+
+// let alpha=prompt('Enter the alphabet')
+// if(alpha=='a'|| alpha=='A' || alpha=='e' || alpha=='E' || alpha=='i' || alpha=='I' || alpha=='O' || alpha=='o'|| alpha=='u' || alpha=='U')
+//     console.log('Entered the vowel')
+// else console.log('Not a vowel')
